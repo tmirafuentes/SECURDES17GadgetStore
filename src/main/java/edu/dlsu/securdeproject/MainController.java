d7a5e2ab9d1ada7d54db3e591707c43d1d259e9e
tmirafuentes <troy_mirafuentes@dlsu.edu.ph>
2018-02-25 16:52:01 +0800

    FIRST PUSH
    I followed this tutorial: https://hellokoding.com/spring-boot-hello-world-example-with-jsp/
    
    Please have Maven downloaded and use IntelliJ when editing here.

---
+++ src/main/java/edu/dlsu/securdeproject/MainController.java
@@ -0,0 +1,20 @@
+package edu.dlsu.securdeproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+    <style type='text/css'>
+    	body {
+    		font-family: Arial;
+    	}
+	</style>
+</head>
+<body>
+    Hello ${name}
+</body>
+</html>
